3db83f2755fb440c5c210629458b4dffc57c2694
Olivia Charles <ocharles92@gmail.com>
2021-04-12 12:47:08 -0400

    User story 3 working and created show page

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -4,11 +4,17 @@ RSpec.describe 'Movies show ' do
   before(:each) do
     @warnerbros = Studio.create(name: "Warner Bros", location: 'Hollywood')
 
-    @harrypotter = @warnerbros.movies.create(title: 'Harry Potter and the Phiosophiers Stone', creation_year: 2001, genre: 'Fantasy')
+    @harrypotter = @warnerbros.movies.create(title: 'Harry Potter and the Philosophers Stone', creation_year: 2001, genre: 'Fantasy')
     @ww = @warnerbros.movies.create(title: 'Wonder Woman 1984', creation_year: 2020, genre: 'Superhero/Action')
     @daniel = Actor.create(name: 'Daniel Ratcliffe',age: 30, currently_working: true)
     @alan = Actor.create(name: 'Alan Rickmane',age: 60, currently_working: false)
     @rupert = Actor.create(name: 'Rupert Grint',age: 32, currently_working: true)
+    @gal = Actor.create(name: 'Gal Gadot',age: 44, currently_working: true)
+
+    MovieCredit.create(actor_id: @daniel.id, movie_id: @harrypotter.id)
+    MovieCredit.create(actor_id: @alan.id, movie_id: @harrypotter.id)
+    MovieCredit.create(actor_id: @rupert.id, movie_id: @harrypotter.id)
+    MovieCredit.create(actor_id: @gal.id, movie_id: @ww.id)
 
     visit "/movies/#{@harrypotter.id}"
   end
@@ -16,13 +22,15 @@ RSpec.describe 'Movies show ' do
   it "shows the movie attributes" do
     expect(page).to have_content(@harrypotter.title)
     expect(page).to have_content(@harrypotter.creation_year)
-    expect(page).to have_content(@harrypotter.genere)
+    expect(page).to have_content(@harrypotter.genre)
     expect(page).to_not have_content(@ww.title)
   end
 
   it "shows the actores in the movie" do
+
     expect(page).to have_content(@daniel.name)
     expect(page).to have_content(@alan.name)
     expect(page).to have_content(@rupert.name)
+    expect(page).to_not have_content(@gal.name)
   end
 end
